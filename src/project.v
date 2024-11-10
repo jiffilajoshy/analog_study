e33288bbd3060e111606f7beeec7fc317f7ce4c5
Rakesh Peter <rakesh.peter@gmail.com>
2024-11-11 00:40:16 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_analog_study (
+module tt_um_vco_mix_dac (
     input  wire       VGND,
     input  wire       VDPWR,    // 1.8v power supply
 //    input  wire       VAPWR,    // 3.3v power supply
